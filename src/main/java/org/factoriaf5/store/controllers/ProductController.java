fb01e589c18d15ca312db3ee35ef4443f9530f35
IngaD89 <inga.demetrashvili.1@iliauni.edu.ge>
2021-05-18 11:56:09 +0200

    test post addProduct

--- src/main/java/org/factoriaf5/store/controllers/ProductController.java
+++ src/main/java/org/factoriaf5/store/controllers/ProductController.java
@@ -2,7 +2,9 @@ package org.factoriaf5.store.controllers;
 
 import org.factoriaf5.store.model.Product;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,8 +21,12 @@ public class ProductController {
     public List<Product> allProducts(){
     return products;
 }
+    public void addProduct(Product product) {
+        products.add(product);
+    }
 
 }
+
 
 
 
